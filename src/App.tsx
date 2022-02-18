439046d2822eaa77bdc827e3f64202f74b4e8665
안병진 <byungjin0120@gmail.com>
2022-02-18 12:41:11 +0900

    Initial Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import Atoms from 'Components/Atoms';
-import Molecules from 'Components/Molecules';
-
 const App = (): JSX.Element => {
-  return (
-    <Atoms.Div>
-      <Atoms.Span>Hello World!!</Atoms.Span>
-      <Molecules.Select />
-    </Atoms.Div>
-  );
+  return <div>hello!</div>;
 };
 
 export default App;
