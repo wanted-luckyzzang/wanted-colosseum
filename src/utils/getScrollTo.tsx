3fa379c1b3594cf41a235fafa6789cc1d6c971a3
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-19 13:39:50 +0900

    chore: getScrollTo 유틸함수 eslint 안되어있어서 적용

--- src/utils/getScrollTo.tsx
+++ src/utils/getScrollTo.tsx
@@ -1,10 +1,7 @@
-const getScrollTo = (
-  type?: string,
-  leftValue?: number,
-) : void => {
-  if (type === "left") {
-    window.scrollTo({ left: leftValue, behavior: 'smooth' })
-  } 
-}
+const getScrollTo = (type?: string, leftValue?: number): void => {
+  if (type === 'left') {
+    window.scrollTo({ left: leftValue, behavior: 'smooth' });
+  }
+};
 
 export default getScrollTo;
